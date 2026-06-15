40ecb86f2022fbddce9297f21369d07f9314074f
vamdevz <devforyou1@gmail.com>
2026-06-15 11:51:02 +0530

    Implement secure backend for Terraform state
    Added a secure backend block for Terraform state management.

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  # ADD THIS SECURE BACKEND BLOCK
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-mgmt"
+    storage_account_name = "sttfstatemanagement2026"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_oidc             = true # Tells Terraform to use our GitHub OIDC trust!
+  }
 }
 
 provider "azurerm" {
@@ -17,13 +26,12 @@ resource "azurerm_resource_group" "lab" {
 }
 
 resource "azurerm_storage_account" "lab_storage" {
-  name                     = "stsecpracticesa2026" # Must be globally unique
+  name                     = "stsecpracticesa2026"
   resource_group_name      = azurerm_resource_group.lab.name
   location                 = azurerm_resource_group.lab.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # SECURITY FLAWS FOR CHECKOV TO CATCH:
-  public_network_access_enabled = true   # Checkov will flag this as too open
-  https_traffic_only_enabled   = false  # Checkov will flag this (unencrypted traffic allowed)
+  public_network_access_enabled = true   
+  https_traffic_only_enabled   = false  
 }
